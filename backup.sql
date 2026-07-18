59429f02d49c47c4f007d115429785749cbc8235
Divine <62357414-divine25@users.noreply.replit.com>
2026-07-18 10:14:59 +0000

    Update project

---
+++ backup.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 5gz6WrzJCuUsDnHzad9COYqXh7QmNUN5qzLfL4NSmc5pHf5bbOLFDbTT6bMLMiw
+
+-- Dumped from database version 16.10
+-- Dumped by pg_dump version 16.10
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 5gz6WrzJCuUsDnHzad9COYqXh7QmNUN5qzLfL4NSmc5pHf5bbOLFDbTT6bMLMiw
+
